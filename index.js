66698753b6134a34ab4f9ef0d5a656b5d11f67bd
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:05:41 +0500

    Step11 Of Typescript
    Step11 Of Typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ["taha", "tayyab", "wasim"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
---
+++ index.ts
@@ -0,0 +1,7 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[] = ["taha", "tayyab", "wasim"]
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
